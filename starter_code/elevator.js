225ec1338a7660fb6dc3c81e79a915a9588f0283
Mercedes Egido <meegido@gmail.com>
2017-09-04 17:28:53 +0200

    iteration 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,40 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.start = setInterval(() => this.update(), 1000)
+
+    }
+
+  stop() {
+    setTimeout (() => clearInterval(this.start), 2000);
+  }
+
+  update() {
+    elevator.log()
+   }
+  //
+  // _passengersEnter() { }
+  //
+  // _passengersLeave() { }
+  //
+  // floorUp() { }
+  //
+  // floorDown() { }
+  //
+  // call() { }
+  //
+  log() {
+    console.log(this.floor, this.direction);
+   }
 }
+
+
+const elevator = new Elevator();
 
 module.exports = Elevator;
