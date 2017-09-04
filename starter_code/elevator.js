1d547d2e47ba9f6c38b0804753777b24c0646d45
Mercedes Egido <meegido@gmail.com>
2017-09-04 18:40:18 +0200

    iteration 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -8,33 +8,44 @@ class Elevator {
 
   start() {
     this.start = setInterval(() => this.update(), 1000)
-
-    }
+  }
 
   stop() {
-    setTimeout (() => clearInterval(this.start), 2000);
+    setTimeout (() => clearInterval(this.start)); //espero a ejecutar por el setTimeout pero puedo ponerlo sin él.
   }
 
   update() {
-    elevator.log()
+    this.log()
    }
+
   //
   // _passengersEnter() { }
   //
   // _passengersLeave() { }
   //
-  // floorUp() { }
-  //
-  // floorDown() { }
-  //
+  floorUp() {
+    if(this.floor === this.MAXFLOOR) {
+      console.log("noooooo")
+    } else {
+      this.floor += 1
+    }
+
+  }
+
+  floorDown() {
+    if(this.floor === 0) {
+      console.log("no puedes bajar más")
+    } else {
+      this.floor -= 1
+    }
+  }
+
   // call() { }
   //
+
   log() {
     console.log(this.floor, this.direction);
    }
 }
-
-
-const elevator = new Elevator();
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -3,4 +3,12 @@ const Person = require('./person.js');
 
 
 const elevator = new Elevator();
+
 elevator.start();
+
+elevator.floorUp()
+let espera = setTimeout(()=> elevator.floorUp(),1000)
+let espera1 = setTimeout(()=> elevator.floorUp(),2000)
+let espera2 = setTimeout(()=> elevator.floorUp(),3000)
+let espera3 = setTimeout(()=> elevator.floorUp(),4000)
+let espera4 = setTimeout(()=> elevator.floorUp(),5000)
